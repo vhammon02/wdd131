60a25f910c149b907297f826945b75a6ae663b11
vhammon02 <ham20032@byui.edu>
2026-04-23 11:37:09 -0600

    added week 1 and home page

---
+++ ponder/W01-JSVariables&Constants/week1.js
@@ -0,0 +1,22 @@
+// JS variables
+let myName = "Von";
+let myAge = 24;
+let myBirthday = "January 1st";
+
+myName = "Von Hammon";
+
+// JS constants
+const  nameOfTheChurch = "The Church of Jesus Christ of Latter-day Saints";
+
+// like print in Python
+
+console.log("My name is " + myName);
+console.log("I am " + myAge + " years old.");
+console.log("My birthday is " + myBirthday + ".");
+console.log("I am a member of " + nameOfTheChurch + ".");
+
+if (myName === "Von Hammon") { 
+    console.log("Welcome, Von Hammon.")
+    } else {
+    console.log("You are not welcome.")
+    }
